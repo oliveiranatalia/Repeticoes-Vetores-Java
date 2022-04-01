ad3d351a263ba0e44c111492c772a3335b38ba8e
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-04-01 12:41:52 -0300

    Adicionar exercicio 1

---
+++ vetores1.java
@@ -0,0 +1,13 @@
+public class vetores1 {
+    public class vetores {
+        public static void main(String[] args) {
+            String[] listaAlunos = new String[3];
+            listaAlunos[0] = "Rodrigo";
+            listaAlunos[1] = "Thay";
+            listaAlunos[2] = "Leandro";
+
+            for(int i = 0; i < listaAlunos.length; i++){
+                System.out.println("Aluno: "+listaAlunos[i]);
+            }
+        }
+}
