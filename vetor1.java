c51c49d2832af5815017cc2486bf45d6fa069566
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-04-18 14:51:52 -0300

    Pedir input

--- vetor1.java
+++ vetor1.java
@@ -1,21 +1,15 @@
-/* Faça um programa que leia 5 valores inteiros, armazene-os em um vetor, 
-calcule e apresente a soma destes valores */
-
-public class vetor1 {
+// Faça um programa que leia 5 valores inteiros, armazene-os em um vetor, calcule e apresente a soma destes valores
+import java.util.Scanner;
+public class vetor01 {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
         int vetorValor[] = new int[5];
         int resultado = 0;
-
-        vetorValor[0] = 3;
-        vetorValor[1] = 90;
-        vetorValor[2] = 1;
-        vetorValor[3] = 8;
-        vetorValor[4] = 2;
-
-
-        for(int i=0; i < vetorValor.length; i++) {
+        for(int i = 0; i < vetorValor.length; i++) {
+            System.out.print("Insira o "+(i+1)+"º número: ");
+            vetorValor[i] = leitor.nextInt();
             resultado = resultado + vetorValor[i];
         }
         System.out.println("O resultado é: "+resultado);
-    }   
+    }
 }
